76a82e0d0586b6b666fece927c26af0951fc0c8c
wangchao <18888351756@163.com>
2022-06-23 17:21:06 +0800

    feat(plugins): on-demand components auto importing for Vue close #1 :zap:

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
 
 createApp(App)
-  .use(Antd)
   .use(createPinia())
   .mount('#app')
