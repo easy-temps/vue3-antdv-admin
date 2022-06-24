78caa2aefc959ffa4ff50f9dfefbc08b38b02c16
wangchao <18888351756@163.com>
2022-06-24 14:52:34 +0800

    chore(deps): set project environment close #2 :tada:

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { visualizer } from 'rollup-plugin-visualizer'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   plugins: [
     vue(),
+    vueJsx(),
+    visualizer(),
     Components({
       resolvers: [AntDesignVueResolver()]
-    })
+    }),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
   ],
 
   server: {
     host: true,
-    port: 3066,
-    open: true,
+    port: 3066
   }
 })
